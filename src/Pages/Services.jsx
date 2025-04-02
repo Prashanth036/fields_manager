e1f4b8ccda432c329530b128b090d63c460f49bc
srikanth-vadla9 <srikanth.vadla@bizcloudexperts.com>
2025-04-02 21:50:08 +0530

    srikanth commit

---
+++ src/Pages/Services.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export const Services = () => {
+    const services_data = [{
+        name:"Live streming service",
+        image:"",
+        about:"Live Stream bio",
+        available_sports:[{icon:"",name:"Badminton"},{icon:"",name:"Table Tennis"},{icon:"",name:"cricket",{icon:"",name:"Chess"}}],
+
+    }];
+  return (
+   
+  )
+}
+
---
+++ src/components/SportsList.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Services } from '../Pages/Services'
+
+export const SportsList = () => {
+  return (
+    <div>
+        
+    </div>
+  )
+}
